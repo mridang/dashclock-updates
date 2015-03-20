719435727fd58f57ddfd36c3022dd78ecf71364a
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2015-03-20 10:20:59 +0200

    Initial commit

--- src/main/java/com/mridang/updates/WidgetSettings.java
+++ src/main/java/com/mridang/updates/WidgetSettings.java
@@ -18,7 +18,8 @@ public class WidgetSettings extends PreferenceActivity {
 	/*
 	 * @see android.preference.PreferenceActivity#onCreate(android.os.Bundle)
 	 */
-	@Override
+	@SuppressWarnings("ConstantConditions")
+    @Override
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
